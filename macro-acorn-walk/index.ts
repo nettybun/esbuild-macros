4a61bac66a94a0278b080a3fdb27724e21fa7280
Gen Hames <hamesg@tutanota.de>
2021-05-03 01:31:00 -0700

    Walking the JS AST

--- macro-acorn-walk/index.ts
+++ macro-acorn-walk/index.ts
@@ -1,8 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unused-vars */
 
 // This is our friend: node_modules/acorn/dist/acorn.d.ts
 import * as acorn from 'acorn';
 import * as walk from 'acorn-walk';
+
+// From collecting all types in a Set() via walk.full(ast, node => ...)
+
+type KnownNodeTypes =
+  | 'BinaryExpression'
+  | 'CallExpression'
+  | 'ExpressionStatement'
+  | 'Identifier'
+  | 'ImportDeclaration'
+  | 'ImportSpecifier'
+  | 'Literal'
+  | 'MemberExpression'
+  | 'Progra'
+  | 'TaggedTemplateExpression'
+  | 'TemplateElement'
+  | 'TemplateLiteral'
+  | 'VariableDeclaration'
+  | 'VariableDeclarator'
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type NodeAny = acorn.Node & { [k: string]: any }
+
 
 // TODO: Import esbuild and use it like macro-regex which transpiles the example
 // code and passes the build result for further work. For now though, here's the
@@ -40,9 +62,50 @@ console.log(t);
 const ast = acorn.parse(bundle, {
   ecmaVersion: 2020,
   sourceType: 'module',
-  locations: true,
+  // This is actually the line/column. I only care about the start/end indices.
+  // locations: true,
 });
 
-walk.full(ast, node => {
-  console.log(node);
-});
+const macroToImports: {
+  [importSource in string]?: {
+    [importSpecifier in string]?: string[]
+  }
+} = {};
+
+// Reverse lookup from node objects?
+// TODO: No. See below.
+const importsToMacro = new Map<NodeAny, { source: string, specifier: string }>();
+
+const walkerMethods: {
+  [key in KnownNodeTypes]?: (node: NodeAny, state: unknown, ancestors: NodeAny[]) => void
+} = {
+  ImportDeclaration(node) {
+    console.log(`Import ${node.start}->${node.end}`);
+    const source = node.source.value as string;
+    (node.specifiers as NodeAny[]).forEach(n => {
+      const o = macroToImports[source] || (macroToImports[source] = {});
+      const a = o[n.imported.name] || (o[n.imported.name] = []);
+      if (!a.includes(n.local.name)) {
+        a.push(n.local.name);
+        importsToMacro.set(n.local, {
+          source,
+          specifier: n.local.name as string,
+        });
+      }
+    });
+  },
+  Identifier(node, state, ancestors) {
+    console.log('Identifier', node.name);
+    // TODO: Ok obviously this isn't how it works - the node stores start/end
+    // information so it'll be different every single time...
+    const meta = importsToMacro.get(node);
+    if (!meta) return;
+    console.log(meta.source, meta.specifier);
+  },
+};
+
+// TODO: https://github.com/acornjs/acorn/issues/946
+// @ts-ignore Doesn't like NodeAny
+walk.ancestor(ast, walkerMethods);
+
+console.log(macroToImports);

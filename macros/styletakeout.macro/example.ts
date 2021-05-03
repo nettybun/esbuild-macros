2c8c8f0e62816337e35fd5a7a79c3a3ccd1a1827
Gen Hames <hamesg@tutanota.de>
2021-05-03 09:45:48 -0700

    Don't let --minify-syntax squash the strings in the example

--- macros/styletakeout.macro/example.ts
+++ macros/styletakeout.macro/example.ts
@@ -12,13 +12,21 @@ const styles = css`
   margin-right: ${decl.size._03};
 `;
 
+// TODO: Ask Evan if this is safe for their upcoming lexer rewrite
+function shadow(css: number) {
+  return css + 10;
+}
+
+console.log(shadow(10));
 console.log(styles);
 console.log(decl.pageBackground);
 console.log(otherStyles);
 
-`m5 p5 ${css`vertical-align: middle`} align-center ${styles} ${classes.text._0_xs}`;
-`m5 p5 ${css`vertical-align: middle`} align-center`;
-`m5 p5 ${styles} ${css`vertical-align: middle`} align-center ${styles}`;
-`${styles} ${css`vertical-align: middle`}`;
-`${css`vertical-align: middle`}`;
-`${css`vertical-align: middle`} hello`;
+// These need to be assigned to a variable else --minify-sytax literally removes
+// the string contents (!) since they're unused.
+const v1 = `m5 p5 ${css`vertical-align: middle`} align-center ${styles} ${classes.text._0_xs}`;
+const v2 = `m5 p5 ${css`vertical-align: middle`} align-center`;
+const v3 = `m5 p5 ${styles} ${css`vertical-align: middle`} align-center ${styles}`;
+const v4 = `${styles} ${css`vertical-align: middle`}`;
+const v5 = `${css`vertical-align: middle`}`;
+const v6 = `${css`vertical-align: middle`} hello`;

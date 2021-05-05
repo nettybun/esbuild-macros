3473ed90949e864e9200595e0426f26d9996045e
Gen Hames <hamesg@tutanota.de>
2021-05-05 09:05:54 -0700

    Learning more about AST walking

--- macro-acorn-walk/acorn-walk.js
+++ macro-acorn-walk/acorn-walk.js
@@ -18,4 +18,43 @@ import { writeFileSync, readFileSync } from 'fs';
 // 'VariableDeclaration'
 // 'VariableDeclarator'
 // Skip esbuild for now since it's not wired up yet
-const ast = readFileSync('./assets/ast.json');
+const ast = JSON.parse(readFileSync('./assets/ast.json', 'utf-8'));
+
+// { "styletakeout.macro": { "decl": ["a1", "d"], "css": ["c", "c1"] }, ... }
+const macroSpecifiersToLocals = {};
+// { "a1": { "source": "styletakeout.macro", "specifier": "decl" }, ... }
+const macroLocalsToSpecifiers = {};
+
+walk.ancestor(ast, {
+  ImportDeclaration(node) {
+    const sourceName = node.source.value;
+    if (!sourceName.endsWith('.macro')) {
+      console.log(`SKIP Import ${node.start}->${node.end}`);
+      return;
+    }
+    console.log(`Import ${node.start}->${node.end} { ${node.specifiers.map(n => `${n.imported.name} as ${n.local.name}`)} } from ${sourceName}`);
+    node.specifiers.forEach(n => {
+      const specImportMap = macroSpecifiersToLocals[sourceName] || (macroSpecifiersToLocals[sourceName] = {});
+      const specLocals = specImportMap[n.imported.name] || (specImportMap[n.imported.name] = []);
+      if (!specLocals.includes(n.local.name)) {
+        specLocals.push(n.local.name);
+        macroLocalsToSpecifiers[n.local.name] = {
+          source: sourceName,
+          specifier: n.imported.name,
+        };
+      }
+    });
+  },
+  // XXX: This is different than `ast.filter(o => o.type === "Indentifier")`
+  // because it _skips_ identifier nodes that are part of variable declarations,
+  // import specifiers, and other areas that _define_ an identifier. This walk
+  // callback is only for the _use_ of identifiers I think...
+  Identifier(node, state, ancestors) {
+    const meta = macroLocalsToSpecifiers[node.name];
+    if (!meta) {
+      console.log(`SKIP Identifier ${node.start}->${node.end}`);
+      return;
+    }
+    console.log(`Indentifier ${node.start}->${node.end} ${node.name} from ${meta.source}:${meta.specifier}`);
+  },
+});

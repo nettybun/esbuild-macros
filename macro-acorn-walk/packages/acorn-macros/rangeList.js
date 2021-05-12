4977d5a8129af7103ec07c1ac3179df07ba27031
Gen Hames <hamesg@tutanota.de>
2021-05-11 21:27:27 -0700

    Easier logic

--- macro-acorn-walk/packages/acorn-macros/rangeList.js
+++ macro-acorn-walk/packages/acorn-macros/rangeList.js
@@ -4,36 +4,43 @@ const ranges = [];
 
 /** @type {(range: Range) => void} */
 const insertRange = (ins) => {
+  if (ins.start > ins.end) {
+    throw new Error('Range start > end');
+  }
   for (let i = 0; i < ranges.length; i++) {
     console.log(`Index ${i}/${ranges.length - 1}`);
     const curr = ranges[i];
-    // TODO: This might be wrong. We want to accept i.s <= c.s && i.e < c.s...
-    if (ins.start < curr.start) {
-      if (ins.end < curr.start) {
-        console.log('Range ins is before curr, inserting; ins,curr', ins, curr);
-        ranges.splice(i, 0, ins);
-        return;
-      } else { // ins.end >= curr.start
-        if (ins.end < curr.end) {
-          console.log('Unexpected partial overlap; ins,curr:', ins, curr);
-          throw new Error('Overlap ins before curr');
-        } else { // ins.end >= curr.end
-          console.log('Range ins is bigger than curr, removing curr; ins,curr', ins, curr);
-          ranges.splice(i, 1);
-        }
-      }
-    } else { // ins.start >= curr.start
-      if (curr.end < ins.start) {
-        console.log('Range ins is after curr, nexting; ins,curr', ins, curr);
-      } else { // curr.end >= ins.start
-        if (curr.end < ins.end) {
-          console.log('Unexpected partial overlap; ins,curr:', ins, curr);
-          throw new Error('Overlap curr before ins');
-        } else { // curr.end >= ins.end
-          console.log('Unexpected ins smaller than curr; ins,curr', ins, curr);
-          throw new Error('Range ins smaller than curr');
-        }
-      }
+    // Entirely _before_ curr.
+    if (ins.start < curr.start && ins.end < curr.start) {
+      console.log('Range ins is before curr, inserting; ins,curr', ins, curr);
+      ranges.splice(i, 0, ins);
+      return;
+    }
+    // Entirely _after_ curr
+    if (ins.start > curr.start && ins.start > curr.end) {
+      console.log('Range ins is after curr, nexting; ins,curr', ins, curr);
+      continue; // Next.
+    }
+    // Overlapping ins into the left of curr
+    if (ins.start < curr.start && ins.end >= curr.start) {
+      console.log('Unexpected partial overlap; ins,curr:', ins, curr);
+      throw new Error('Overlap');
+    }
+    // Overlapping ins into the right of curr
+    if (ins.start >= curr.start && ins.end >= curr.end) {
+      console.log('Unexpected partial overlap; ins,curr:', ins, curr);
+      throw new Error('Overlap');
+    }
+    // Covering entire curr by ins
+    if (ins.start <= curr.start && ins.end >= curr.end) {
+      console.log('Range ins is bigger than curr, removing curr; ins,curr', ins, curr);
+      ranges.splice(i, 1);
+      continue; // Next.
+    }
+    // Covered entire ins by curr
+    if (ins.start >= curr.start && ins.end <= curr.end) {
+      console.log('Unexpected ins smaller than curr; ins,curr', ins, curr);
+      throw new Error('Range ins smaller than curr');
     }
   }
 };
